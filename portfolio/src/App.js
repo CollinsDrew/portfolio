f2d476a5e0a0d63a9fe93ffb13835f05f111408d
CollinsDrew <collinsdrew1990@gmail.com>
2023-02-04 09:12:41 +0000

    skills component started and styled

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,15 @@
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
+      <Skills />
     </div>
   );
 }
---
+++ portfolio/src/Components/Skills.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import JavaScript from "../Assests/javascript.png";
+import ReactImg from "../Assests/react.png";
+import Node from "../Assests/node.png";
+import HTMLImg from "../Assests/html.png";
+import cssImg from "../Assests/css-1.png";
+import mongo from "../Assests/mongo.png";
+import GitHub from "../Assests/github.png";
+import tailwindImg from "../Assests/tailwind.png";
+import cypressImg from "../Assests/cypressIoLogo.png";
+
+const Skills = () => {
+  return (
+    <div name="skills" className="bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Experience
+          </p>
+          <p className="py-4">// These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTMLImg} alt="HTML Icon" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={cssImg} alt="CSS Icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="JS Icon" />
+            <p className="my-4">JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React.js Icon" />
+            <p className="my-4">React.js</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="Node.js Icon" />
+            <p className="my-4">Node.js / Express.js</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={mongo} alt="Mongo Icon" />
+            <p className="my-4">Mongo.DB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={tailwindImg}
+              alt="Tailwind Icon"
+            />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={cypressImg} alt="Cypress Icon" />
+            <p className="my-4">Cypress.io</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
